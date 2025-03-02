1f1e8fd88458e34971fdf7b3dbc65c99b457c780
Jørund Jenssen Berg <jjberg@gmail.com>
2025-03-02 11:33:03 +0100

    Updated entire folder with new structure - Intellisense survived!

---
+++ Fundamentals/Program.cs
@@ -0,0 +1,5 @@
+﻿using Fundamentals.Working_with_text;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Fundamentals!");
+ExerciseLoop.Run();
---
+++ Fundamentals/Working_with_text/ExerciseLoop.cs
@@ -0,0 +1,9 @@
+namespace Fundamentals.Working_with_text;
+
+public class ExerciseLoop
+{
+    public static void Run()
+    {
+        Console.WriteLine("This is ExerciseLoop.cs running!");
+    }
+}
