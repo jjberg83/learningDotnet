a23a9c6b05b18343ed1392d6e1a80a74d63af671
Jørund Jenssen Berg <jjberg@gmail.com>
2025-03-13 06:45:28 +0100

    Started new chapter about files
    Need to build Menu Loop Logic for the chapters also

--- Fundamentals/Program.cs
+++ Fundamentals/Program.cs
@@ -1,5 +1,7 @@
 ﻿using Fundamentals.Working_with_text;
+using Fundamentals.Working_with_files;
 
 // See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Fundamentals!");
-WorkingWithText_MenuLoop.Run();
+//WorkingWithText_MenuLoop.Run();
+WorkingWithFiles_MenuLoop.Run();
---
+++ Fundamentals/Working_with_files/WorkingWithFiles_MenuLoop.cs
@@ -0,0 +1,68 @@
+namespace Fundamentals.Working_with_files;
+
+public class WorkingWithFiles_MenuLoop
+{
+    public static void Run()
+    {
+        Console.WriteLine("You are now in the loop");
+        /*
+        bool loopButton = true;
+        while (loopButton)
+        {
+            Console.WriteLine("##################");
+            Console.WriteLine("Please choose an exercise between 1-5 OR the name of the file you want to run (type 'Exit' to stop)");
+            Console.WriteLine("##################");
+            Console.WriteLine();
+
+            string? userInput = Console.ReadLine();
+            
+            switch(userInput)
+            {
+                case "1":
+                    Console.WriteLine("-----------------");
+                    Console.WriteLine("Starting Exercise 1");
+                    Console.WriteLine("-----------------");
+                    Console.WriteLine();
+                    WorkingWithText_Exercises_ProceduralProgrammingStyle.Exercise1();
+                    break;
+                case "2":
+                    Console.WriteLine("-----------------");
+                    Console.WriteLine("Starting Exercise 2");
+                    Console.WriteLine("-----------------");
+                    Console.WriteLine();
+                    WorkingWithText_Exercises_ProceduralProgrammingStyle.Exercise2();
+                    break;
+                case "3":
+                    Console.WriteLine("-----------------");
+                    Console.WriteLine("Starting Exercise 3");
+                    Console.WriteLine("-----------------");
+                    Console.WriteLine();
+                    WorkingWithText_Exercises_ProceduralProgrammingStyle.Exercise3();
+                    break;
+                case "4":
+                    Console.WriteLine("-----------------");
+                    Console.WriteLine("Starting Exercise 4");
+                    Console.WriteLine("-----------------");
+                    Console.WriteLine();
+                    WorkingWithText_Exercises_ProceduralProgrammingStyle.Exercise4();
+                    break;
+                case "5":
+                    Console.WriteLine("-----------------");
+                    Console.WriteLine("Starting Exercise 5");
+                    Console.WriteLine("-----------------");
+                    Console.WriteLine();
+                    WorkingWithText_Exercises_ProceduralProgrammingStyle.Exercise5();
+                    break;
+                case "Exit":
+                case "exit":
+                    Console.WriteLine("Stopping program...");
+                    loopButton = false;
+                    break;
+                default:
+                    Console.WriteLine("You did not write a number between 1-5. Program stopping...");
+                    loopButton = false;
+                    break;
+            }
+        }*/
+    }
+}
