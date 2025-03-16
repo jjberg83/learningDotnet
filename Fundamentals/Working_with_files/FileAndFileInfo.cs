f527e884113cc1129b33c6aa252da7dbdcc55fc6
Jørund Jenssen Berg <jjberg@gmail.com>
2025-03-16 11:04:46 +0100

    Bro! Sjekk for at fil kopieres
    Klarer ikke se den i sidemenyen på VSCode

--- Fundamentals/Working_with_files/FileAndFileInfo.cs
+++ Fundamentals/Working_with_files/FileAndFileInfo.cs
@@ -30,11 +30,17 @@ public class FileAndFileInfo
 
         if (copy.Exists)
         {
-            Thread.Sleep(3000);
-            copy.Delete();
+            Console.WriteLine("Kopi skal eksistere");
+            // Console.WriteLine("Kopi slettes...");
+            // Thread.Sleep(3000);
+            // copy.Delete();
         }
 
-        FileInfo fileToLoadTextFrom = new FileInfo(root + "joshWaitzkin.txt"); 
-        System.IO.FileStream joshWaitzkinQuote = fileToLoadTextFrom.OpenRead(); // Han sa denne var litt kompleks, og han har rett....
+        // FileInfo fileToLoadTextFrom = new FileInfo(root + "joshWaitzkin.txt"); 
+        // System.IO.FileStream joshWaitzkinQuote = fileToLoadTextFrom.OpenRead(); // Han sa denne var litt kompleks, og han har rett....
+
+        string joshWaitzkinQuote = File.ReadAllText(root + "joshWaitzkin.txt"); // Laster det bare inn i variabelen
+        Console.WriteLine(joshWaitzkinQuote);
+        Console.WriteLine();
     }
 }
---
+++ Fundamentals/Working_with_files/WorkingWithFiles_Exercises_Copy.cs
@@ -0,0 +1,25 @@
+namespace Fundamentals.Working_with_files;
+
+public class WorkingWithFiles_Exercises
+{
+    public static void Exercise1()
+    {
+        Console.WriteLine("Inside Exercise 1");
+    }
+    public static void Exercise2()
+    {
+        Console.WriteLine("Inside Exercise 2");
+    }
+    public static void Exercise3()
+    {
+        Console.WriteLine("Inside Exercise 3");
+    }
+    public static void Exercise4()
+    {
+        Console.WriteLine("Inside Exercise 4");
+    }
+    public static void Exercise5()
+    {
+        Console.WriteLine("Inside Exercise 5");
+    }
+}
