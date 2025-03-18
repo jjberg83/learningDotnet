64fd8e8d922b14db022300a543a36a8f87c35205
Jørund Jenssen Berg <jjberg@gmail.com>
2025-03-18 18:12:14 +0100

    Bro! Added Edabit section for Edabit challenges.

---
+++ EdabitChallenges/Challenges.cs
@@ -0,0 +1,12 @@
+namespace EdabitChallenges;
+
+public static class Challenges
+{
+    public static void Stuttering()
+    {
+        Console.WriteLine("Please enter a word:");
+        string? userInput = Console.ReadLine();
+        if()
+        Console.WriteLine($"You entered {userInput}");
+    }
+}
---
+++ EdabitChallenges/Program.cs
@@ -0,0 +1,17 @@
+﻿using EdabitChallenges;
+
+Console.WriteLine("What challenge do you want to run?");
+string? userInput = Console.ReadLine();
+
+switch (userInput)
+{
+    case "Stuttering":
+        Challenges.Stuttering();
+        break;
+    default:
+        Console.WriteLine("No such directory");
+        break;
+}
+
+
+
