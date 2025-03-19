6b3a8f5691f95496b2590bee86313b2efd793f44
Jørund Jenssen Berg <jjberg@gmail.com>
2025-03-19 06:48:40 +0100

    Nearly finisehd with lecture on Directory and DirectoryInfo

--- Fundamentals/Working_with_files/DirectoryAndDirectoryInfo.cs
+++ Fundamentals/Working_with_files/DirectoryAndDirectoryInfo.cs
@@ -12,6 +12,28 @@ class DirectoryAndDirectoryInfo
             // Her kommer innholdet i filen
             sw.WriteLine("// Bare en kommentar foreløpig");
         }
-        Directory.GetFiles(root, ".cs", SearchOption.AllDirectories);
+        // string[] files = Directory.GetFiles(root + "/", ".cs", SearchOption.AllDirectories);
+        // (parameter 1: hvilket directory, noen spesielle filer?, rekursivt?)
+        string[] files = Directory.GetFiles(root ,"*.cs*", SearchOption.AllDirectories);
+        
+        foreach(string file in files)
+        {
+            Console.WriteLine(file);
+        }
+
+        // Så gjør vi akkurat det samme for Directories
+        string[] directories = Directory.GetDirectories(root, "*.*", SearchOption.AllDirectories);
+
+        Console.WriteLine("#############################");
+        foreach(string directory in directories)
+        {
+            Console.WriteLine(directory);
+        }
+
+        bool mappe1Finnes = Directory.Exists(root + "/Directory_skapt_i_VSCode");
+        bool mappe2Finnes = Directory.Exists(root + "/Directory_som_ikke_finnes");
+        Console.WriteLine($"Finnes {root}/Directory_skapt_i_VSCode? {mappe1Finnes}");
+        Console.WriteLine($"Finnes {root}/Directory_som_ikke_finnes? {mappe2Finnes}");
+
     }
 }
