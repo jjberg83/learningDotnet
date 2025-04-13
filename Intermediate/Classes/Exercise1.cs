59d1e77390fb0811c9db8937957a7f75aabc5a41
Jørund Jenssen Berg <jjberg@gmail.com>
2025-04-13 11:51:19 +0200

    Stoppeklokke laget, men må testes

--- Intermediate/Classes/Exercise1.cs
+++ Intermediate/Classes/Exercise1.cs
@@ -1,8 +1,49 @@
+using System.Collections;
+
 namespace Intermediate.Classes;
 public class Exercise1
 {
     public static void Run()
     {
-        Console.WriteLine("Stop watch challenge accepted!");
+        Stopwatch stoppeKlokke = new Stopwatch();
+        bool fortsettTidtakning = true;
+        Console.WriteLine("-------------------");
+        Console.WriteLine("Stoppeklokke program startet!");
+        Console.WriteLine("Skriv følgende for å bruke stoppeklokken:");
+        Console.WriteLine("START = start en ny runde på stoppeklokken");
+        Console.WriteLine("STOPP = stopp runden du allerede har startet");
+        Console.WriteLine("SLUTT = slutt stoppeklokke progammet");
+        Console.WriteLine("-------------------");
+        Console.WriteLine();
+        string? userInput;
+        while(fortsettTidtakning)
+        {
+            userInput = Console.ReadLine();
+
+            if(string.IsNullOrWhiteSpace(userInput))
+            {
+                Console.WriteLine("Husk å skrive enten START, STOPP eller SLUTT");
+                continue;
+            }
+        
+            userInput = userInput.ToUpper();
+            
+            switch(userInput)
+            {
+                case "START":
+                    stoppeKlokke.Start();
+                    break;
+                case "STOPP":
+                    stoppeKlokke.Stop();
+                    break;
+                case "SLUTT":
+                    fortsettTidtakning = false;
+                    Console.WriteLine("Stoppeklokkeprogram avsluttes...");
+                    break;
+                default:
+                    Console.WriteLine("Du har ikke skrevet enten START, STOPP eller SLUTT");
+                    break;
+            }
+        }
     }
 }
--- Intermediate/Classes/Stopwatch.cs
+++ Intermediate/Classes/Stopwatch.cs
@@ -1,10 +1,13 @@
+using System.Reflection.Metadata;
+using System.Runtime.Intrinsics.X86;
+
 namespace Intermediate.Classes;
 
 // Exercise 1: Design a Stopwatch
 // Design a class called Stopwatch.The job of this class is to simulate a stopwatch.It should
 // provide two methods: Start and Stop.We call the start method first, and the stop method next.
 // Then we ask the stopwatch about the duration between start and stop. Duration should be a
-// value in TimeSpan.Display the duration on the console.
+// value in TimeSpan. Display the duration on the console.
 // We should also be able to use a stopwatch multiple times.So we may start and stop it and then
 // start and stop it again. Make sure the duration value each time is calculated properly.
 // We should not be able to start a stopwatch twice in a row (because that may overwrite the initial
@@ -21,11 +24,21 @@ public class Stopwatch
 {
     public List<DateTime> StartTimes { get; private set; } = new List<DateTime>();
     public List<DateTime> StopTimes { get; private set; } = new List<DateTime>();
+    public List<TimeSpan> Durations { get; private set; } = new List<TimeSpan>();
     public void Start()
     {
         // Sjekk først at lengden på StartTimes og lengden på StopTimes er like
         // Hvis ulike, kan man ikke gå videre og får beskjed om å stoppe først
-        // Finn ut tiden akkurat når start metoden startes, og legg denne til i StartTimes        
+        // Finn ut tiden akkurat når start metoden startes, og legg denne til i StartTimes
+        if(StartTimes.Count != StopTimes.Count)
+        {
+            Console.WriteLine("Du kan ikke starte en ny runde før du har avsluttet forrige. Husk å skriv STOPP i terminal vinduet.");
+            return;
+        }
+        DateTime startTid = DateTime.Now;     
+        StartTimes.Add(startTid);
+        Console.WriteLine("Stoppeklokken har startet!");
+        Console.WriteLine();
     }
     public void Stop()
     {
@@ -33,8 +46,22 @@ public class Stopwatch
         // Hvis ikke får man beskjed om dette (altså at man må starte først)
         // Når stop passerer denne sjekken, finnes tiden akkurat nå, og timespannet vises,
         // Stop tiden legges til i stop listen.
-
-        // Kan jo tømme listene etter hver gang. Ikke nødvendig å fylle opp. Bare nødvendig for en gangs bruk og så tømme.
-        // Kanskje readonly var en dum ide?
+        if(StopTimes.Count != StartTimes.Count - 1)
+        {
+            Console.WriteLine("Du kan ikke stoppe noe siden du ikke har startet en runde enda. Husk å skrive START i terminal vinduet.");
+            return;
+        }
+        DateTime stoppTid = DateTime.Now;
+        StopTimes.Add(stoppTid);
+        TimeSpan varighet = stoppTid - StartTimes[StartTimes.Count - 1];
+        Durations.Add(varighet);
+        var minutter = varighet.Minutes;
+        var sekunder = Math.Round(varighet.TotalSeconds, 2, MidpointRounding.AwayFromZero);
+        
+        Console.WriteLine($"sekunder: {sekunder}");
+        Console.WriteLine($"varighet: {varighet}");
+        Console.WriteLine();
+        Console.WriteLine("-------------------------");
+        Console.WriteLine();
     }
 }
