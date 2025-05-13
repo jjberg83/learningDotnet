186447a6aca57c6e9db51b633e0c196e4442187c
Jørund Jenssen Berg <jjberg@gmail.com>
2025-05-13 18:54:31 +0200

    Fullførte Exercise Stack

--- Intermediate/Inheritance/Exercise.cs
+++ Intermediate/Inheritance/Exercise.cs
@@ -1,3 +1,5 @@
+using System.Runtime.InteropServices;
+
 namespace Intermediate.Inheritance;
 
 internal class Exercise
@@ -5,11 +7,25 @@ internal class Exercise
     public static void Run()
     {
         var stack = new Stack();
-        stack.Push(1);
-        stack.Push(2);
-        stack.Push(3);
-        Console.WriteLine(stack.Pop()); // -> Bør gi 3
-        Console.WriteLine(stack.Pop());// -> Bør gi 2
-        Console.WriteLine(stack.Pop());// -> Bør gi 1
+        int ini = 1;
+        int max = 5;
+
+        for(int i = ini; i <= max; i++)
+        {
+            stack.Push(i);
+        }
+        for(int i = ini; i <= max; i++)
+        {
+            Console.WriteLine(stack.Pop()); 
+        }
+        // stack.Push(1);
+        // stack.Push(2);
+        // stack.Push(3);
+        // Console.WriteLine(stack.Pop()); // -> Bør gi 3
+        // Console.WriteLine(stack.Pop());// -> Bør gi 2
+        // Console.WriteLine(stack.Pop());// -> Bør gi 1
+
+        // Should fail
+        stack.Push(null);
     } 
 }
--- Intermediate/Inheritance/Stack.cs
+++ Intermediate/Inheritance/Stack.cs
@@ -5,34 +5,25 @@ internal class Stack
     // Lister bør alltid ha readonly og initialiseres her eller i konstruktør
     // Da kan liste-instansen aldri lages på ny, og alt dens innhold slettes ved et uhell
     // men listens elementer kan likevel legges til og trekkes fra
-    // MEN, siden Clear metoden skal slette alt innholdet, bruker jeg ikke readonly under
-    // Da måtte jeg loopet gjennom listen og slettet hvert element, som er costly
-    // Fieldet er privat uansett, så jeg trenger kun følge med på potensielle farlige
-    // nye opprettelser av instanser her inne (som vil slette hele listens innhold)
-    private List<object> _elements;
+    private readonly List<object> _elements = new();
     public void Push(object obj)
     {
         if(obj == null)
-            throw new InvalidOperationException();
-        if(_elements.Count == 0)
-            _elements = new();
+            throw new InvalidOperationException("You cannot add a null object to the stack");
         _elements.Add(obj);
     }
 
     public object Pop()
     {
         if(_elements.Count == 0)
-            throw new InvalidOperationException();
-        else
-        {
-            object objectToBeRemoved = _elements[-1];
-            _elements.RemoveAt(-1);
-            return objectToBeRemoved;
-        }
+            throw new InvalidOperationException("You cannot Pop an element from an empty list");
+        object objectToBeRemoved = _elements[_elements.Count - 1];
+        _elements.RemoveAt(_elements.Count - 1);
+        return objectToBeRemoved;
     }
 
     public void Clear()
     {
-        _elements = new();
+        _elements.Clear();
     }
 }
