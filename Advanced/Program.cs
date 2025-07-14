45659dbbf6df151c094823f5285ca875a3e08792
Jørund Jenssen Berg <jjberg@gmail.com>
2025-07-14 05:58:01 +0200

    Lagde nytt Advanced prosjekt (og kjører hele prosjektet med reflections)

---
+++ Advanced/Program.cs
@@ -0,0 +1,38 @@
+﻿bool loopButton = true;
+while (loopButton)
+{
+    Console.WriteLine("##################");
+    Console.WriteLine("Skriv namespace.filnavn på det du vil kjøre (skriv stopp for å avslutte):");
+    Console.WriteLine("##################");
+    Console.WriteLine();
+
+    string? userInput = Console.ReadLine();
+
+    if (userInput != null && userInput.ToLower() == "stopp")
+    {
+        Console.WriteLine("Avslutter program...");
+        break;
+    }
+
+    string className = $"Advanced.{userInput}";
+
+    Type? type = Type.GetType(className);
+
+    if (type != null)
+    {
+        var runMethod = type.GetMethod("Run", System.Reflection.BindingFlags.Public | System.Reflection.BindingFlags.Static);
+        if (runMethod != null)
+        {
+            runMethod.Invoke(null, null);
+        }
+        else
+        {
+            Console.WriteLine($"Klassen '{userInput}' har ikke en statisk Run() metode.");
+        }
+    }
+    else
+    {
+        Console.WriteLine($"Fant ingen klasse med navn '{userInput}.'");
+    }
+}
+
