c3d9d346f8e37ec08cae9d66ce54b7dbb017d57d
Jørund Jenssen Berg <jjberg@gmail.com>
2025-07-18 10:46:15 +0200

    Begynt på Generics leksjonen
    Først litt repetisjon på boxing(value types til heap, altså reference types) og unboxing(reference types til stack, altså value types)

---
+++ Advanced/Generics/Generics.cs
@@ -0,0 +1,26 @@
+namespace Advanced.Generics;
+
+public class Generics
+{
+    public static void Run()
+    {
+        // Først litt repetisjon som er relevant for å forklare hvorfor Generics er bra
+
+        // Boxing er når en value type konverteres til en reference type
+        // Selv om 7 er int, er alle ints også objects, så dette går
+        object objekt = 7;
+
+        // Unboxing er når vi går andre veien, altså at reference types
+        // konverteres til value types
+        int tall = (int)objekt;
+
+        // Det ligner veldig på upcasting og downcasting siden, men der så
+        // vi på base classes og derived classes. Her skifter vi mellom
+        // reference og value types. Når man har boxing lagrer man en value
+        // type i heapen (ved å bokse det inn i en referansetype). Når 
+        // man unboxer, tar man ut value typen fra reference typen. 
+
+        // Det har en kostnad fordi man må allokere plass av og på i heapen, 
+        // i tillegg til man må sjekke typer.
+    }
+}
